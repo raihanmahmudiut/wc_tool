16120af5ef1b646a41154724e945c2847cbd6b7f
Raihan Mahmud <raihanmahmudiut@gmail.com>
2024-02-22 22:27:32 +0600

    added final functionalities

--- src/main.rs
+++ src/main.rs
@@ -22,28 +22,58 @@ fn count_lines_words_chars_bytes<R: BufRead>(reader: R) -> (usize, usize, usize,
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
+    let default_file = "text.txt";
 
-    if args.len() != 3 {
+    if args.len() == 0 || args.len() > 3 {
         println!("Usage: {} <option> <file>", args[0]);
         println!("Options: -b, -l, -w, -c");
         return;
     }
 
-    let option = &args[1];
-    let file_path = &args[2];
+    let file_path = if args.len() == 3 {
+        &args[2]
+    } else {
+        default_file
+    };
 
     match File::open(file_path) {
         Ok(file) => {
             let reader = BufReader::new(file);
             let (lines, words, chars, bytes) = count_lines_words_chars_bytes(reader);
 
-            match option.as_str() {
-                "-b" => println!("Bytes: {}", bytes),
-                "-l" => println!("Lines: {}", lines),
-                "-w" => println!("Words: {}", words),
-                "-c" => println!("Characters: {}", chars),
+            if args.len() == 1 {
+                println!("Lines: {}", lines);
+                println!("Words: {}", words);
+                println!("Chars: {}", chars);
+                println!("Bytes: {}", bytes);
+            }
 
-                _ => println!("Invalid option: {}", option),
+            if args.len() == 3 {
+                match args[1].as_str() {
+                    "-l" => println!("Lines: {}", lines),
+                    "-w" => println!("Words: {}", words),
+                    "-c" => println!("Chars: {}", chars),
+                    "-b" => println!("Bytes: {}", bytes),
+                    _ => println!("Usage: {} <option> <file>", args[0]),
+                }
+            }
+
+            if args.len() == 2 {
+                match args.len() {
+                    2 => match args[1].as_str() {
+                        "-l" => println!("Lines: {}", lines),
+                        "-w" => println!("Words: {}", words),
+                        "-c" => println!("Chars: {}", chars),
+                        "-b" => println!("Bytes: {}", bytes),
+                        _ => println!("Invalid option"),
+                    },
+                    _ => {
+                        println!("Lines: {}", lines);
+                        println!("Words: {}", words);
+                        println!("Chars: {}", chars);
+                        println!("Bytes: {}", bytes);
+                    }
+                }
             }
         }
         Err(_) => eprintln!("wc: {}: No such file or directory", file_path),
